668f306f597a7a84116777686513596d117fffad
lidonghai <jacle169@21cn.com>
2018-07-08 13:51:02 +0800

    初始化

---
+++ Coolpy7_Esp8266_Upload_DataPoint/Coolpy7_Esp8266_Upload_DataPoint.ino
@@ -0,0 +1,62 @@
+#include <ESP8266WiFi.h>
+#include <MQTT.h>
+
+const char ssid[] = "Xiaomi_1111";
+const char pass[] = "20001937";
+
+WiFiClientSecure net;
+MQTTClient client;
+
+unsigned long lastMillis = 0;
+
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect("arduino", "try", "try")) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+
+  client.subscribe("/hello");
+  // client.unsubscribe("/hello");
+}
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, pass);
+
+  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
+  // You need to set the IP address directly.
+  //
+  // MQTT brokers usually use port 8883 for secure connections.
+  client.begin("broker.shiftr.io", 8883, net);
+  client.onMessage(messageReceived);
+
+  connect();
+}
+
+void loop() {
+  client.loop();
+  delay(10);  // <- fixes some issues with WiFi stability
+
+  if (!client.connected()) {
+    connect();
+  }
+
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 3000) {
+    lastMillis = millis();
+    client.publish("/hello", "world");
+  }
+}
